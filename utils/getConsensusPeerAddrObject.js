7df187d33db568c1dfaa11e0274724a4b99e5164
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-02-13 23:29:01 +0000

    Added /consensusPeerAddr endpoint

--- utils/getConsensusPeerAddrObject.js
+++ utils/getConsensusPeerAddrObject.js
@@ -3,8 +3,17 @@ function getConsensusPeerAddrObject(poolMap) {
   
   // Iterate through all clients in the poolMap
   for (const [clientId, client] of poolMap) {
+    // Skip if client or required fields are missing
+    if (!client || !client.id || !client.enr) continue;
+
+    // Create the peer address object for each client
+    const peerAddrObj = {
+      machineID: client.id,
+      consensusPeerAddr: client.enr,
+      consensusClient: client.consensus_client ? client.consensus_client.split(' ')[0].toLowerCase() : 'unknown'
+    };
     
-    // Add this code to get the peerId
+    consensusPeerAddr.push(peerAddrObj);
   }
 
   return { consensusPeerAddr };
