69184daa003e5375f5e74af183ab14fd535638b1
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-02-17 21:07:59 +0000

    Renamed pool nodes log file

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const poolPort = 3003;
 const wsHeartbeatInterval = 30000; // 30 seconds
 const socketTimeout = 500;
 
-const poolNodeLogPath = "/home/ubuntu/shared/poolNode.log";
+const poolNodeLogPath = "/home/ubuntu/shared/poolNodes.log";
 
 module.exports = {
   portPoolPublic,
