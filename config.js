d1871df270f67abda6ac42750f07c7c12647d0f3
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-02-24 18:56:41 +0000

    Changed log name

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 const portPoolPublic = 48546;
 const poolPort = 3003;
 const wsHeartbeatInterval = 30000; // 30 seconds
-// const socketTimeout = 500;
-const socketTimeout = 10000;
+const socketTimeout = 500;
 const pointUpdateInterval = 10000;
 
 const poolNodeLogPath = "/home/ubuntu/shared/poolNodes.log";
-const compareResultsLogPath = "/home/ubuntu/shared/compareResults.log";
+const compareResultsLogPath = "/home/ubuntu/shared/poolCompareResults.log";
 
 module.exports = {
   portPoolPublic,
