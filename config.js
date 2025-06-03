17b215c42591ac6dc0e62d97823b0a575d7c848f
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-06-03 16:11:22 +0000

    Changed spot check only threshold:

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const socketTimeout = 500;
 const pointUpdateInterval = 10000;
 const requestSetChance = 5; // 1 in n requests will be a set request
 // const requestSetChance = 1000000000000; // 1 in n requests will be a set request
-const spotCheckOnlyThreshold = 250; // The 75th percentile cuttoff for node response time (last week) that excludes nodes from handling single requests (milliseconds)
+const spotCheckOnlyThreshold = 150; // The 75th percentile cuttoff for node response time (last week) that excludes nodes from handling single requests (milliseconds)
 const nodeTimingFetchInterval = 24 * 60 * 60 * 1000; // Interval for a forced fetching of node timing data (24 hours)
 const cacheUpdateInterval = 1000; // Interval for updating the cache (1 second)
 
