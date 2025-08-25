5ce92970ba7ddb660df75d800b2aa66e0d7529ca
Ubuntu <ubuntu@ip-172-31-34-98.ec2.internal>
2025-08-25 14:57:16 +0000

    Re-enabled response checks

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const nodeMethodSpecificTimeouts = {
   'eth_getBlockByHash': 1100
 };
 const pointUpdateInterval = 10000;
-// const requestSetChance = 5; // 1 in n requests will be a set request
-const requestSetChance = 1000000000000; // 1 in n requests will be a set request
+const requestSetChance = 5; // 1 in n requests will be a set request
+// const requestSetChance = 1000000000000; // 1 in n requests will be a set request
 const spotCheckOnlyThreshold = 150; // The 75th percentile cuttoff for node response time (last week) that excludes nodes from handling single requests (milliseconds)
 const nodeTimingFetchInterval = 24 * 60 * 60 * 1000; // Interval for a forced fetching of node timing data (24 hours)
 const poolNodeStaleThreshold = 5 * 60 * 1000; // 5 minutes Timeout threshold for stale nodes in poolMap
