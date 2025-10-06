6f15ac7bd03637a8481179744bc40903427af5dd
sfaber34 <sfaber34@users.noreply.github.com>
2025-10-06 16:47:34 +0000

    Lowered fast node threshold to 250 ms

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const nodeMethodSpecificTimeouts = {
 const pointUpdateInterval = 10000;
 // const requestSetChance = 5; // 1 in n requests will be a set request
 const requestSetChance = 1000000000000; // 1 in n requests will be a set request
-const spotCheckOnlyThreshold = 1000; // The 75th percentile cuttoff for node response time (last week) that excludes nodes from handling single requests (milliseconds)
+const spotCheckOnlyThreshold = 250; // The 75th percentile cuttoff for node response time (last week) that excludes nodes from handling single requests (milliseconds)
 const nodeTimingFetchInterval = 24 * 60 * 60 * 1000; // Interval for a forced fetching of node timing data (24 hours)
 const poolNodeStaleThreshold = 5 * 60 * 1000; // 5 minutes Timeout threshold for stale nodes in poolMap
 
